0812064964de578c86416301de5f3812d7f6c2a3
Patrick Hulin <patrick.hulin@gmail.com>
2026-04-07 14:31:39 -0400

    fix packages, add wrangler

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 const phasermsg = () => {
 	return {
@@ -27,16 +27,6 @@ export default defineConfig({
 				manualChunks: {
 					phaser: ["phaser"],
 				},
-			},
-		},
-		minify: "terser",
-		terserOptions: {
-			compress: {
-				passes: 2,
-			},
-			mangle: true,
-			format: {
-				comments: false,
 			},
 		},
 	},
